6dde4fc44376357eeed402ca5b1b16e598d30839
Vikram Babu <dme.weekend.batch01.drive01@gmail.com>
2022-05-01 06:52:06 +0530

    init

--- micro-service-starter/docker_code/ui-web-app-reactjs/src/components/App.jsx
+++ micro-service-starter/docker_code/ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://172.31.10.65:9999/'+e.target.name;
+    const url = 'http://65.0.131.90:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
